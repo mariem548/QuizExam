c2095d385b3f2fd7390e4cb0b2eb40ca1a651997
BENSALAHmyriam <maryem.bslh@gmail.com>
2024-11-06 12:57:16 +0100

    correction de texte

--- quiz/src/api/api.js
+++ quiz/src/api/api.js
@@ -1,7 +1,7 @@
 export const fetchCategories = async () => {
   const responseCategorie = await fetch("https://opentdb.com/api_category.php");
-  if (!responseCategorie) {
-    return new Error(" Probleme lors de chargement des categories ");
+  if (!responseCategorie.ok) {
+    return new Error(" Problème lors de chargement des categories ");
   }
   const data = await responseCategorie.json();
   return data.trivia_categories;
@@ -12,7 +12,7 @@ export const fetchQuestions = async () => {
     "https://opentdb.com/api.php?amount=5&category=11&difficulty=easy&type=multiple"
   );
   if (!responseQuestions.ok) {
-    throw new Error(" Probleme lors de récuperation des questions");
+    throw new Error(" Problème lors de récuperation des questions");
   }
   const data = await responseQuestions.json();
   console.log("data", data.results);
